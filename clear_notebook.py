9e57b384909ac81dd6b405f831eb99cba4df9609
L4L4a <elviskenneth123@gmail.com>
2025-10-06 07:09:04 -0500

    🛠 Add notebook-cleanup helper and finalize project files

---
+++ clear_notebook.py
@@ -0,0 +1,11 @@
+﻿import json, sys
+p = sys.argv[1]
+nb = json.load(open(p, 'r', encoding='utf-8'))
+for cell in nb.get("cells", []):
+    if cell.get("cell_type") == "code":
+        # clear outputs and execution_count
+        cell["outputs"] = []
+        cell["execution_count"] = None
+# write back
+json.dump(nb, open(p, 'w', encoding='utf-8'), indent=1)
+print("Cleared outputs in:", p)
